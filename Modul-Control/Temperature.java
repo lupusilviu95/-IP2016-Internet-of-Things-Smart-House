8f041a481f3d730990798167edc0dbd6db8df7e1
Adamars <mihai_petrea2000@yahoo.com>
2016-04-26 12:34:08 +0300

    Added files via upload

---
+++ Modul-Control/Temperature.java
@@ -0,0 +1,9 @@
+package com.example.nicoletatanasa.internetofthings;
+/**
+ * Created by Nicoleta Tanasa on 24-Apr-16.
+ */
+public interface Temperature {
+
+    public int getCurrentTemperature();
+
+}
