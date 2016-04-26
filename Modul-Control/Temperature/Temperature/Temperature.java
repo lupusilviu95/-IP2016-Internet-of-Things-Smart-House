4c766ea69249324bffa4db8e4401afff1c9a0950
Adamars <mihai_petrea2000@yahoo.com>
2016-04-26 12:43:50 +0300

    Delete Temperature.java

--- Modul-Control/Temperature/Temperature/Temperature.java
+++
@@ -1,9 +0,0 @@
-package com.example.nicoletatanasa.internetofthings;
-/**
- * Created by Nicoleta Tanasa on 24-Apr-16.
- */
-public interface Temperature {
-
-    public int getCurrentTemperature();
-
-}
