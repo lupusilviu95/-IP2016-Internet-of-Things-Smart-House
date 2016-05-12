367e58e0abdd78e67c3f2614b2e72874ec8ea81a
Adamars <mihai_petrea2000@yahoo.com>
2016-05-12 08:25:39 +0300

    Delete HumidityController.java

--- Modul-Control/Humidity/HumidityController.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Madalina on 24-Apr-16.
- */
-public interface HumidityController {
-    public void setCurrentStatus(boolean currentStatus);
-    public boolean setStatus(boolean status);
-}
